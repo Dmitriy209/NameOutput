662c111ee452c04a589d97577c8ba4cb08cc26cd
Dmitriy209 <dim286754@gmail.com>
2024-06-19 19:55:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int symbolsLength = 2;
+
+            Console.WriteLine("Введите символ:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+
+            string lastLines = "";
+            string middleLine = Convert.ToString(symbol) + name + Convert.ToString(symbol);
+            
+            int lineLength = name.Length + symbolsLength;
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                lastLines += Convert.ToString(symbol);
+            }
+
+            Console.WriteLine(lastLines);
+            Console.WriteLine(middleLine);
+            Console.WriteLine(lastLines);
+        }
+    }
+}
